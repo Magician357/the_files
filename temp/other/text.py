cf0ef8580ec9e4276683e26abba1be5d4da60e3e
Magic Man <65839659+Magician357@users.noreply.github.com>
2023-09-11 13:52:34 -0700

    add a program

---
+++ temp/other/text.py
@@ -0,0 +1,128 @@
+a=3
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+l=3
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+b=5
+# amazing code!
+# amazing code!
+# amazing code!
+m=5
+# amazing code!
+# amazing code!
+# amazing code!
+C=print
+# amazing code!
+# amazing code!
+# amazing code!
+assert 1==1
+# amazing code!
+# amazing code!
+# amazing code!
+from string import ascii_letters as E,digits as F
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+"need more money"
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+c=20
+d=1
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+for _ in range(d):
+    print("",end="")
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+B=range
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+from random import choice as D,randint as A
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+from time import sleep as Z
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+G=[A for A in E+F]
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+for H in B(A(d,a)):
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# C=1
+        C('loading...')
+        # amazing code!
+# amazing code!
+# amazing code!
+        # amazing code!
+# amazing code!
+            # amazing code!
+        for H in B(A(a*b,b*c)):C(''.join(D(G)for A in B(A(b,a*b))));Z(.1)
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+# amazing code!
+C('finished')
