13b9e6eddac4bea2e953036b43b889146a4c993a
Magic Man <65839659+Magician357@users.noreply.github.com>
2023-09-12 13:29:16 -0700

    add more text, make python worse to read

--- temp/other/text.py
+++ temp/other/text.py
@@ -1,128 +1,347 @@
 a=3
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-l=3
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+b=18
+B=5
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+l=15
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+l/=B
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 b=5
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 m=5
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 C=print
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 assert 1==1
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 from string import ascii_letters as E,digits as F
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+P=F.join(E.split())
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 "need more money"
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+ #
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 c=20
 d=1
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+B=13
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 for _ in range(d):
     print("",end="")
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 B=range
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 from random import choice as D,randint as A
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 from time import sleep as Z
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 G=[A for A in E+F]
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 for H in B(A(d,a)):
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 # C=1
         C('loading...')
-        # amazing code!
-# amazing code!
-# amazing code!
-        # amazing code!
-# amazing code!
-            # amazing code!
+        # the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+        # the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+            # the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
         for H in B(A(a*b,b*c)):C(''.join(D(G)for A in B(A(b,a*b))));Z(.1)
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
-# amazing code!
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
+# the aloryingm is efficient
+
+# wX4e1Gbh6Cw1cI4
 C('finished')
